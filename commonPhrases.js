9352013ed6cf4c373ce59b07a1207f3f1b9c2006
lkraman <laurakraman01@gmail.com>
2019-04-30 16:36:35 -0400

    initial commit

---
+++ commonPhrases.js
@@ -0,0 +1,31 @@
+exports.askForBathroomEnglish = function(){
+ console.log("May I please use your restroom?");
+}
+
+exports.askForDirectionsEnglish = function(){
+ console.log("Could you please tell me how to get to <insert destination here>?");
+}
+
+exports.askForBathroomJapanese = function(){
+ console.log("toire wa doko desu ka");
+}
+
+exports.askForDirectionsJapanese = function(){
+ console.log("Michijun ni tsuite");
+}
+
+exports.askForBathroomIcelandic = function(){
+ console.log("Hvar er klósettið?");
+}
+
+exports.askForDirectionsIcelandic = function(){
+ console.log("fyrir leiðbeiningar");
+}
+
+exports.askForBathroomSpanish = function(){
+ console.log("¿dónde están los baños?,");
+}
+
+exports.askForDirectionsSpanish = function(){
+ console.log("Cómo puedo llegar a?");
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const commonPhrases = require("./commonPhrases.js");
+
+greetings.greetingEnglish();
+
+greetings.greetingJapanese();
+
+greetings.greetingIcelandic();
+
+greetings.greetingSpanish();
+
+commonPhrases.askForBathroomEnglish();
+
+commonPhrases.askForDirectionsEnglish();
+
+commonPhrases.askForBathroomJapanese();
+
+commonPhrases.askForDirectionsJapanese();
+
+commonPhrases.askForBathroomIcelandic();
+
+commonPhrases.askForDirectionsIcelandic();
+
+commonPhrases.askForBathroomSpanish();
+
+commonPhrases.askForDirectionsSpanish();
